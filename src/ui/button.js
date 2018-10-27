e997b780addf0ea55e8dc2749f004bf59e954923
Adrián Chacón <adrianchaconmadueno@gmail.com>
2018-10-27 11:23:42 +0200

    Setup button in Storybook

--- src/ui/button.js
+++ src/ui/button.js
@@ -1,7 +1,14 @@
 import styled from 'styled-components'
 
 const Button = styled.button`
-background: red;
+background: black;
+border-radius: 3px;
+border:2px solid white;
+color: white;
+padding: 10px;
+font-size: 16px;
+font-family: 'VT323', monospace;
+min-width: 100px;
 `
 
 export { Button }

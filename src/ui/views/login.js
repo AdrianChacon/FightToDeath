9e1cfe45f832fefa01a67038d4d0c5fa04c6968f
Fran Bosquet <fran@redradix.com>
2018-10-27 22:00:59 +0200

    :shirt: Fix linter error

--- src/ui/views/login.js
+++ src/ui/views/login.js
@@ -5,25 +5,25 @@ import { Title } from '../components/title'
 import { Label } from '../components/label'
 import { VerticalLayout, HorizontalLayout } from '../components/layouts'
 
-const Login = ({username, password, onChangeUsername, onChangePassword}) => {
+const Login = ({ username, password, onChangeUsername, onChangePassword }) => {
 	return (
-    <>
-      <VerticalLayout height='20vh'>
-      	<Title align='center'>Fight To Death</Title>
-      </VerticalLayout>
-	<VerticalLayout height='60vh' justify='center' padding>
-		<Label>E-mail</Label>
-		<Input placeholder='E-mail' value={username} onChange={onChangeUsername}/>
-		<Label>Password</Label>
-		<Input type='password' placeholder='Password' value={password} onChange={onChangePassword}/>
-	</VerticalLayout>
-	<VerticalLayout height='20vh'>
-		<HorizontalLayout justify='space-around'>
-			<Button>Login</Button>
-			<Button>Sign Up</Button>
-		</HorizontalLayout>
-	</VerticalLayout>
-    </>
+	<>
+		<VerticalLayout height='20vh'>
+			<Title align='center'>Fight To Death</Title>
+		</VerticalLayout>
+		<VerticalLayout height='60vh' justify='center' padding>
+			<Label>E-mail</Label>
+			<Input placeholder='E-mail' value={ username } onChange={ onChangeUsername }/>
+			<Label>Password</Label>
+			<Input type='password' placeholder='Password' value={ password } onChange={ onChangePassword }/>
+		</VerticalLayout>
+		<VerticalLayout height='20vh'>
+			<HorizontalLayout justify='space-around'>
+				<Button>Login</Button>
+				<Button>Sign Up</Button>
+			</HorizontalLayout>
+		</VerticalLayout>
+  </>
 	)
 }
 
--- src/views/Login.js
+++ src/views/Login.js
@@ -1,40 +1,40 @@
-import React from 'react'
 import { Login } from '../ui/views/login'
 import { withStateHandlers, setDisplayName, compose } from 'recompose'
 
-class LoginEnhanced extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            username: '',
-            password: ''
-        }
-        this.onChangeUsername = event => this.setState({ username: event.target.value })
-        this.onChangePassword = event => this.setState({ password: event.target.value })
+// TODO: Remove legacy code, kept it just to compare with hocs
+// class LoginEnhanced extends React.Component{
+// 	constructor(props){
+// 		super(props)
+// 		this.state = {
+// 			username: '',
+// 			password: ''
+// 		}
+// 		this.onChangeUsername = event => this.setState({ username: event.target.value })
+// 		this.onChangePassword = event => this.setState({ password: event.target.value })
 
-    }
+// 	}
 
-    render() {
-        return (
-            <Login
-            username={ this.state.username }
-            password={ this.state.password }
-            onChangeUsername={this.onChangeUsername}
-            onChangePassword={this.onChangePassword}
-            />
-        )
-    }
-}
+// 	render() {
+// 		return (
+// 			<Login
+// 				username={ this.state.username }
+// 				password={ this.state.password }
+// 				onChangeUsername={ this.onChangeUsername }
+// 				onChangePassword={ this.onChangePassword }
+// 			/>
+// 		)
+// 	}
+// }
 
 const enhancer = compose(
-    setDisplayName('LoginEnhanced'),
-    withStateHandlers({
-        username: '',
-        password: ''
-        },{
-        onChangeUsername: () => event => ({username: event.target.value}),
-        onChangePassword: () => event => ({password: event.target.value})
-    })
-   )
+	setDisplayName('LoginEnhanced'),
+	withStateHandlers({
+		username: '',
+		password: ''
+	},{
+		onChangeUsername: () => event => ({ username: event.target.value }),
+		onChangePassword: () => event => ({ password: event.target.value })
+	})
+)
    
 export default enhancer(Login)

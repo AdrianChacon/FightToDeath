f9e375f7ddbdbeb8fea23c638503c47c31aefd11
Fran Bosquet <fran@redradix.com>
2018-11-01 12:03:42 +0100

    Add storybook viewports

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,17 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import { configureViewport, INITIAL_VIEWPORTS, withViewport } from '@storybook/addon-viewport'
+
+configureViewport({
+  viewports: INITIAL_VIEWPORTS,
+  defaultViewport: 'iphone6'
+})
 
 const req = require.context('../src/ui', true, /.stories.js$/);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+addDecorator(withViewport('iphone6'))
 
 configure(loadStories, module);
